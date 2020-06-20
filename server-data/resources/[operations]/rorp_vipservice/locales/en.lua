5aa982f87b6f168d15ccd4e91b57183267496636
whysaputro <hendrawahyu26@gmail.com>
2020-06-21 00:52:34 +0700

    no message

--- server-data/resources/[operations]/rorp_vipservice/locales/en.lua
+++ server-data/resources/[operations]/rorp_vipservice/locales/en.lua
@@ -1,3 +1,3 @@
 Locales['en'] = {
-  ['access_reward_center'] = 'Tekan ~INPUT_PICKUP~ untuk membuka menu Pengambilan Hadiah.',
+  ['access_reward_center'] = 'Tekan ~INPUT_PICKUP~ untuk membuka menu VIP Service.',
 }
