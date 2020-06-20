087389ec61bc58024c7a47d5179609f52b6cfd74
whysaputro <hendrawahyu26@gmail.com>
2020-06-20 15:25:55 +0700

    no message

--- server-data/resources/[operations]/rorp_daftarwarga/fxmanifest.lua
+++ server-data/resources/[operations]/rorp_daftarwarga/fxmanifest.lua
@@ -10,32 +10,15 @@ server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'config.lua',
-	'locales/de.lua',
-	'locales/br.lua',
 	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/cs.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
 	'config.lua',
-	'locales/de.lua',
-	'locales/br.lua',
 	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/cs.lua',
-	'client/main.lua',
-	'client/utils.lua'
+	'client/main.lua'
 }
 
 dependency 'es_extended'
