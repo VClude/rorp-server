123bb155f7375dccabeb33c29c84015470c2d734
whysaputro <hendrawahyu26@gmail.com>
2020-06-21 21:14:34 +0700

    menambahkan gcphone

---
+++ server-data/resources/[phone]/gcphone/sql/base.sql
@@ -0,0 +1,38 @@
+ALTER TABLE users ADD COLUMN phone_number VARCHAR(10) NULL;
+
+CREATE TABLE IF NOT EXISTS phone_app_chat (
+id int(11) NOT NULL AUTO_INCREMENT,
+channel varchar(20) NOT NULL,
+message varchar(255) NOT NULL,
+time timestamp NOT NULL DEFAULT current_timestamp(),
+PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS phone_calls (
+id int(11) NOT NULL AUTO_INCREMENT,
+owner varchar(10) NOT NULL COMMENT 'Name',
+num varchar(10) NOT NULL COMMENT 'Phone Number',
+incoming int(11) NOT NULL COMMENT 'Define Incoming Call',
+time timestamp NOT NULL DEFAULT current_timestamp(),
+accepts int(11) NOT NULL COMMENT 'Accept Call',
+PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=122 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS phone_messages (
+id int(11) NOT NULL AUTO_INCREMENT,
+transmitter varchar(10) NOT NULL,
+receiver varchar(10) NOT NULL,
+message varchar(255) NOT NULL DEFAULT '0',
+time timestamp NOT NULL DEFAULT current_timestamp(),
+isRead int(11) NOT NULL DEFAULT 0,
+owner int(11) NOT NULL DEFAULT 0,
+PRIMARY KEY (id)
+) ENGINE=MyISAM AUTO_INCREMENT=106 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS phone_users_contacts (
+id int(11) NOT NULL AUTO_INCREMENT,
+identifier varchar(60) CHARACTER SET utf8mb4 DEFAULT NULL,
+number varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL,
+display varchar(64) CHARACTER SET utf8mb4 NOT NULL DEFAULT '-1',
+PRIMARY KEY (id)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
