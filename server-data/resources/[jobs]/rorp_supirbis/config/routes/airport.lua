ff3fc37ae47db88e9d367de47d870b79cdf120d2
whysaputro <hendrawahyu26@gmail.com>
2020-06-24 03:18:16 +0700

    pergantian rute bis jobcenter

--- server-data/resources/[jobs]/rorp_supirbis/config/routes/airport.lua
+++ server-data/resources/[jobs]/rorp_supirbis/config/routes/airport.lua
@@ -10,16 +10,14 @@ AirportRoute = {
             Name = 'airport_route',
             BusColor = 5,
             Stops = {
-                {x = -1664.8002, y = -538.1304,  z = 35.2513, name = 'stop_banner_hotel', unloadType = UnloadType.None},
-                {x = -1860.3963, y = -350.8236,  z = 49.6338, name = 'stop_von_crastenburg', unloadType = UnloadType.None},
-                {x = -1031.2937, y = -2729.3339, z = 13.7566, name = 'stop_airport', unloadType = UnloadType.All},
-                {x = -1304.3708, y = -407.5448,  z = 35.5009, name = 'stop_elgin', unloadType = UnloadType.Some},
-                {x = -1333.0507, y = -1089.8925, z = 6.9793,  name = 'stop_venetian', unloadType = UnloadType.Some},
-                {x = -1031.2937, y = -2729.3339, z = 13.7566, name = 'stop_airport', unloadType = UnloadType.All},
-                {x = -824.8701,  y = -1221.3138, z = 7.3654,  name = 'stop_viceroy', unloadType = UnloadType.Some},
-                {x = -880.8395,  y = -2110.7658, z = 9.0868,  name = 'stop_crastenburg', unloadType = UnloadType.Some},
-                {x = -695.0627,  y = -2283.1557, z = 13.0476, name = 'stop_opium_nights', unloadType = UnloadType.Some},
-                {x = -1031.2937, y = -2729.3339, z = 13.7566, name = 'stop_airport', unloadType = UnloadType.All},
+                {x = -1034.18, y = -2729.91, z = 20.08, name = 'stop_airport', unloadType = UnloadType.None},
+                {x = 309.62, y = -1377.16,  z = 31.81, name = 'stop_driving_school', unloadType = UnloadType.None},
+                {x = 276.82, y = -580.35, z = 42.89, name = 'stop_rumah_sakit', unloadType = UnloadType.All},
+                {x = -392.49, y = 1202.8,  z = 325.64, name = 'stop_job_center', unloadType = UnloadType.Some},
+                {x = 224.14, y = 1240.34, z = 225.22,  name = 'stop_transit_bus', unloadType = UnloadType.Some},
+                {x = 251.56, y = -574.26, z = 42.96, name = 'stop_rumah_sakit2', unloadType = UnloadType.All},
+                {x = 253.01,  y = -1502.8, z = 28.9,  name = 'stop_driving_school2', unloadType = UnloadType.Some},
+                {x = -1034.18,  y = -2729.91, z = 20.08,  name = 'stop_crastenburg', unloadType = UnloadType.Some},
             }
         }
     }
