b611cd09b95d2053f63fd12bb3ec77ce8ec64ee0
whysaputro <hendrawahyu26@gmail.com>
2020-06-24 14:22:01 +0700

    tambah rental perahu

---
+++ server-data/resources/[rental]/rorp_boat-rental/config.lua
@@ -0,0 +1,34 @@
+Config                            = {}
+Config.Locale                     = 'en'
+
+--- #### BASICS
+Config.EnablePrice = true -- false = bikes for free
+Config.EnableEffects = false
+Config.EnableBlips = true
+
+
+--- #### PRICES	
+Config.PriceTriBike = 89
+Config.PriceScorcher = 99
+Config.PriceCruiser = 129
+Config.PriceBmx = 109
+
+
+--- #### MARKER EDITS
+Config.TypeMarker = 1
+Config.MarkerScale = {{x = 1.000,y = 2.000,z = 1.000}}
+Config.MarkerColor = {{r = 0,g = 255,b = 255}}
+	
+Config.MarkerZones = { 
+
+    {x = -1263.02,y = -1439.86,z = 4.35},
+
+}
+
+
+-- Edit blip titles
+Config.BlipZones = { 
+
+    {title="Rental Perahu", colour=7, x = -731.02, y = -1309.17, z = 5.0},
+
+}
---
+++ server-data/resources/[rental]/rorp_boat-rental/translations/en.lua
@@ -0,0 +1,20 @@
+Locales['en'] = {
+	['press_e'] = 'Tekan ~INPUT_CONTEXT~ untuk menyewa sepeda.',
+	['storebike'] = 'Tekan ~INPUT_CONTEXT~ untuk mengembalikan sepeda.',	
+	['biketitle'] = 'Rental Sepeda',
+	['bike'] = 'Bike - <span style="color:green;">TriBike</span> <span style="color:red;">89$</span>',
+	['bike2'] = 'Bike - <span style="color:green;">Scorcher</span> <span style="color:red;">99$</span>',
+	['bike3'] = 'Bike - <span style="color:green;">Cruiser</span> <span style="color:red;">129$</span>',
+	['bike4'] = 'Bike - <span style="color:green;">BMX</span> <span style="color:red;">109$</span>',
+	['bikefree'] = 'Bike - <span style="color:green;">TriBike</span>',
+	['bike2free'] = 'Bike - <span style="color:green;">Scorcher</span>',
+	['bike3free'] = 'Bike - <span style="color:green;">Cruiser</span>',
+	['bike4free'] = 'Bike - <span style="color:green;">BMX</span>',
+	['bikemessage'] = 'Kami harap anda menikmatinya. Silahkan datang lagi kembali. :)',
+	['notabike'] = 'Kamu tidak dalam menggunakan sepeda!',
+	
+	['bike_pay'] = 'Kamu telah membayar: $%s',
+	
+	['bikes'] = '[Bikes]',
+	
+}
