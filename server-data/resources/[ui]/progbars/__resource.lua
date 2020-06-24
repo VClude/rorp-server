0ecdb5d6c0e17e494bb2a3481cfe843e951c4f48
whysaputro <hendrawahyu26@gmail.com>
2020-06-24 09:42:11 +0700

    mengganti file IPL dari bob74_ipl menjadi fivem-ipl

---
+++ server-data/resources/[ui]/progbars/__resource.lua
@@ -0,0 +1,20 @@
+-- Found on: https://modit.store
+-- ModFreakz Discord: https://discord.gg/4S7FcFs
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page 'index.html'
+
+client_scripts {
+  'client.lua',
+} 
+
+files {
+  'index.html',
+  'MarvelRegular-Dj83.TTF',
+}
+
+exports {
+  'StartProg',
+  'CloseProg',
+}
