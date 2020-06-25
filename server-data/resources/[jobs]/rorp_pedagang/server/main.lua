39fd5686280406788401f9fc60ecff135581ee73
whysaputro <hendrawahyu26@gmail.com>
2020-06-25 19:33:04 +0700

    no message

--- server-data/resources/[jobs]/rorp_pedagang/server/main.lua
+++ server-data/resources/[jobs]/rorp_pedagang/server/main.lua
@@ -25,7 +25,7 @@ AddEventHandler('rorp_pedagang:cooking', function(ingredients)
 	else
 		if xPlayer ~= nil then
 			if hasAllIngredients(xPlayer.inventory, Config.Recipes[item]) then
-				ESX.TriggerEventCallback('rorp_pedagang:checkSpace', function(hasSpace)
+				ESX.TriggerServerCallback('rorp_pedagang:checkSpace', function(hasSpace)
 					if hasSpace then
 						for _,ingredient in pairs(Config.Recipes[item]) do
 							if (ingredient.remove ~= nil and ingredient.remove) or (ingredient.remove == nil) then
