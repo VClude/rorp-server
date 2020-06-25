7e6a0560b22db3712cbe598a786c0640dab20433
whysaputro <hendrawahyu26@gmail.com>
2020-06-25 11:38:04 +0700

    no message

--- server-data/resources/[jobs]/rorp_pedagang/client/main.lua
+++ server-data/resources/[jobs]/rorp_pedagang/client/main.lua
@@ -82,6 +82,21 @@ AddEventHandler("rorp_pedagang:hasExitedMarker", function(zone)
     end
 end)
 
+function DrawText3Ds(x, y, z, text)
+	local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+	local px, py, pz = table.unpack(GetGameplayCamCoords())
+
+	SetTextScale(0.35, 0.35)
+	SetTextProportional(1)
+	SetTextColour(255, 255, 255, 255)
+	SetTextEntry("STRING")
+	SetTextFont(6)
+	SetTextCentre(1)
+	AddTextComponentString(text)
+	DrawText(_x, _y)
+	local factor = (string.len(text)) / 240
+	DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 0, 0, 0, 150)
+end
 
 -- Create Blips
 
