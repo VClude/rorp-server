7c26774a26cd107016bcfda9aa1d60c2da486fc0
whysaputro <hendrawahyu26@gmail.com>
2020-06-25 12:02:25 +0700

    no message

--- server-data/resources/[jobs]/esx_ambulancejob/config.lua
+++ server-data/resources/[jobs]/esx_ambulancejob/config.lua
@@ -1,94 +1,129 @@
 Config                            = {}
+Config                            = {}
 
 Config.DrawDistance               = 100.0
 
-Config.Marker                     = {type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = true}
+Config.Marker                     = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, a = 100, rotate = false }
 
-Config.ReviveReward               = 700  -- revive reward, set to 0 if you don't want it enabled
+Config.ReviveReward               = 500  -- revive reward, set to 0 if you don't want it enabled
 Config.AntiCombatLog              = true -- enable anti-combat logging?
-Config.LoadIpl                    = false -- disable if you're using fivem-ipl or other IPL loaders
+Config.LoadIpl                    = true -- disable if you're using fivem-ipl or other IPL loaders
 
 Config.Locale                     = 'en'
 
-Config.EarlyRespawnTimer          = 60000 * 1  -- time til respawn is available
-Config.BleedoutTimer              = 60000 * 10 -- time til the player bleeds out
+local second = 1000
+local minute = 60 * second
+
+Config.EarlyRespawnTimer          = 3 * minute  -- Time til respawn is available
+Config.BleedoutTimer              = 3 * minute -- Time til the player bleeds out
 
 Config.EnablePlayerManagement     = true
+Config.EnableSocietyOwnedVehicles = true
 
-Config.RemoveWeaponsAfterRPDeath  = false
-Config.RemoveCashAfterRPDeath     = false
-Config.RemoveItemsAfterRPDeath    = false
+Config.RemoveWeaponsAfterRPDeath  = true
+Config.RemoveCashAfterRPDeath     = true
+Config.RemoveItemsAfterRPDeath    = true
 
 -- Let the player pay for respawning early, only if he can afford it.
-Config.EarlyRespawnFine           = false
+Config.EarlyRespawnFine           = true
 Config.EarlyRespawnFineAmount     = 5000
 
-Config.RespawnPoint = {coords = vector3(365.35, -593.44, 43.28), heading = 63.99}
+Config.RespawnPoint = { coords = vector3(341.07, -1397.31, 32.51), heading = 48.5}
 
 Config.Hospitals = {
 
 	CentralLosSantos = {
 
 		Blip = {
-			coords = vector3(317.3, -590.66, 43.28),
+			coords = vector3(321.06, -563.66, 28.79),
 			sprite = 61,
-			scale  = 1.2,
+			scale  = 1.0,
 			color  = 2
 		},
 
 		AmbulanceActions = {
-			vector3(299.15, -598.12, 42.28),
-			vector3(334.8, -592.72, 42.28)
+			vector3(336.23, -579.94, 28.79) --336.23, -579.94, 28.79
 		},
 
 		Pharmacies = {
-			vector3(344.71, -592.45, 42.28)
+			vector3(321.06, -563.66, 27.79) --321.06, -563.66, 28.79
 		},
 
 		Vehicles = {
 			{
-				Spawner = vector3(299.05, -573.07, 43.26),
-				InsideShop = vector3(290.37, -561.75, 43.26),
-				Marker = {type = 36, x = 1.0, y = 1.0, z = 1.0, r = 100, g = 50, b = 200, a = 100, rotate = true},
+				Spawner = vector3(330.11, -549.57, 28.74), --330.11,-549.57,28.74
+				InsideShop = vector3(330.11, -549.57, 28.74),
+				Marker = { type = 36, x = 1.0, y = 1.0, z = 1.0, r = 100, g = 50, b = 200, a = 100, rotate = true },
 				SpawnPoints = {
-					{coords = vector3(291.88, -573.33, 43.2), heading = 61.22, radius = 4.0},
-					{coords = vector3(291.88, -573.33, 43.2), heading = 61.22, radius = 4.0},
-					{coords = vector3(291.88, -573.33, 43.2), heading = 61.22, radius = 4.0}
+					{ coords = vector3(330.11, -549.57, 28.74), heading = 227.6, radius = 4.0 },
+					{ coords = vector3(330.11, -549.57, 28.74), heading = 227.6, radius = 4.0 },
+					{ coords = vector3(330.11, -549.57, 28.74), heading = 227.6, radius = 6.0 }
 				}
 			}
 		},
 
 		Helicopters = {
 			{
-				Spawner = vector3(345.64, -580.65, 74.16),
-				InsideShop = vector3(350.91, -587.78, 74.16),
-				Marker = {type = 34, x = 1.5, y = 1.5, z = 1.5, r = 100, g = 150, b = 150, a = 100, rotate = true},
+				Spawner = vector3(317.5, -1449.5, 46.5),
+				InsideShop = vector3(305.6, -1419.7, 41.5),
+				Marker = { type = 34, x = 1.5, y = 1.5, z = 1.5, r = 100, g = 150, b = 150, a = 100, rotate = true },
 				SpawnPoints = {
-					{coords = vector3(350.91, -587.78, 74.16), heading = 248.66, radius = 10.0},
-					{coords = vector3(350.91, -587.78, 74.16), heading = 248.66, radius = 10.0}
+					{ coords = vector3(313.5, -1465.1, 46.5), heading = 142.7, radius = 10.0 },
+					{ coords = vector3(299.5, -1453.2, 46.5), heading = 142.7, radius = 10.0 }
 				}
 			}
 		},
 
 		FastTravels = {
-			{
-				From = vector3(294.7, -1448.1, 29.0),
-				To = {coords = vector3(272.8, -1358.8, 23.5), heading = 0.0},
-				Marker = {type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
-			}
-		},
+            {
+                From = vector3(294.7, -1448.1, 29.0),
+                To = { coords = vector3(272.8, -1358.8, 23.5), heading = 0.0 },
+                Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
+            },
+
+            {
+                From = vector3(275.3, -1361, 23.5),
+                To = { coords = vector3(295.8, -1446.5, 28.9), heading = 0.0 },
+                Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
+            },
+
+            {
+                From = vector3(247.3, -1371.5, 23.5),
+                To = { coords = vector3(333.1, -1434.9, 45.5), heading = 138.6 },
+                Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
+            },
+
+            {
+                From = vector3(335.5, -1432.0, 45.50),
+                To = { coords = vector3(249.1, -1369.6, 23.5), heading = 0.0 },
+                Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
+            },
+
+            {
+                From = vector3(234.5, -1373.7, 20.9),
+                To = { coords = vector3(320.9, -1478.6, 28.8), heading = 0.0 },
+                Marker = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, a = 100, rotate = false }
+            },
+
+            {
+                From = vector3(317.9, -1476.1, 28.9),
+                To = { coords = vector3(238.6, -1368.4, 23.5), heading = 0.0 },
+                Marker = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, a = 100, rotate = false }
+            }
+	    },
 
 		FastTravelsPrompt = {
 			{
-				From = vector3(329.81, -601.02, 42.28),
-				To = {coords = vector3(338.96, -583.99, 74.16), heading = 0.0},
-				Marker = {type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false},
+				From = vector3(237.4, -1373.8, 26.0),
+				To = { coords = vector3(251.9, -1363.3, 38.5), heading = 0.0 },
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false },
 				Prompt = _U('fast_travel')
 			},
+
 			{
-				From = vector3(338.96, -583.99, 73.16),
-				To = {coords = vector3(329.81, -601.02, 42.28), heading = 0.0},
-				Marker = {type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false},
+				From = vector3(256.5, -1357.7, 36.0),
+				To = { coords = vector3(235.4, -1372.8, 26.3), heading = 0.0 },
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false },
 				Prompt = _U('fast_travel')
 			}
 		}
@@ -97,39 +132,66 @@ Config.Hospitals = {
 }
 
 Config.AuthorizedVehicles = {
-	car = {
-		ambulance = {
-			{model = 'ambulance', price = 5000}
-		},
 
-		doctor = {
-			{model = 'ambulance', price = 4500}
+	ambulance = {
+		{
+			model = 'ambulance',
+			label = 'אמבולנס',
+			price = 10
 		},
-
-		chief_doctor = {
-			{model = 'ambulance', price = 3000}
-		},
-
-		boss = {
-			{model = 'ambulance', price = 2000}
+		{
+			model = 'ambulance22',
+			label = 'אמבולנס2',
+			price = 10
+		}
+	},
+	
+	ambulance = {
+		{
+			model = 'lgaurd',
+			label = 'אמבולנס',
+			price = 10
+		}
+	},
+	
+	ambulance = {
+		{
+			model = 'vortex',
+			label = 'אופנוע',
+			price = 10
 		}
 	},
 
-	helicopter = {
-		ambulance = {},
+	doctor = {
+		{
+			model = 'ambulance',
+			label = 'אמבולנס',
+			price = 10
+		}
+	},
 
-		doctor = {
-			{model = 'buzzard2', price = 150000}
-		},
+	chief_doctor = {
+		{
+			model = 'ambulance',
+			label = 'אמבולנס',
+			price = 10
+		}
+	},
 
-		chief_doctor = {
-			{model = 'buzzard2', price = 150000},
-			{model = 'seasparrow', price = 300000}
-		},
-
-		boss = {
-			{model = 'buzzard2', price = 10000},
-			{model = 'seasparrow', price = 250000}
+	boss = {
+		{
+			model = 'ambulance',
+			label = 'אמבולנס',
+			price = 10
 		}
 	}
+
+}
+
+Config.AuthorizedHelicopters = {
+
+	ambulance = {},
+
+	
+
 }
