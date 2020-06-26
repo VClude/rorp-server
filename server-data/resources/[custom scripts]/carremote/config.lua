e60e03609d94cb249a6044af893b4fe05a367d01
whysaputro <hendrawahyu26@gmail.com>
2020-06-26 16:07:38 +0700

    no message

--- server-data/resources/[custom scripts]/carremote/config.lua
+++ server-data/resources/[custom scripts]/carremote/config.lua
@@ -1,8 +1,8 @@
 Config                      = {}
 
 Config.Locale               = 'en'
-Config.HotkeyUI             = 7   -- Main Hotkey For Opening UI / Locking Vehicles
-Config.HotkeyEngine         = 303   -- Sencondary Hotkey For Turning Engine On/Off
+Config.HotkeyUI             = 314   -- Main Hotkey For Opening UI / Locking Vehicles
+Config.HotkeyEngine         = 315   -- Sencondary Hotkey For Turning Engine On/Off
 Config.SwitchDistance       = 3     -- How Close You Need To Be To Switch Connected Vehicles
 Config.MaxRemoteRange       = 30    -- Set max range that the remote will work.
 Config.MaxAlarmDistance     = 50    -- Set max distance the alarm audio can be heard from
@@ -12,4 +12,4 @@ Config.MaxFobBeepVolume     = .20   -- Set max volume for the fob beep audio
 
 Config.UseRemoteRange       = true  -- Set whether you want to use the maximum remote range feature
 Config.UseBattery           = true  -- Set whether you are using the battery on the keyfob or not
-Config.StealableMotorcycles = false  -- Set whether you want to be realistic and make motorcycles stealable
+Config.StealableMotorcycles = true  -- Set whether you want to be realistic and make motorcycles stealable
