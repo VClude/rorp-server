4c18fb7e1fdb19fd6cbeace8daf832a60e76aa76
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 00:35:30 +0700

    no message

--- server-data/resources/[needs]/esx_basicneeds/client/main.lua
+++ server-data/resources/[needs]/esx_basicneeds/client/main.lua
@@ -40,13 +40,13 @@ end)
 AddEventHandler('esx_status:loaded', function(status)
 
 	TriggerEvent('esx_status:registerStatus', 'hunger', 1000000, '#CFAD0F', function(status)
-		return false
+		return true
 	end, function(status)
 		status.remove(100)
 	end)
 
 	TriggerEvent('esx_status:registerStatus', 'thirst', 1000000, '#0C98F1', function(status)
-		return false
+		return true
 	end, function(status)
 		status.remove(75)
 	end)
