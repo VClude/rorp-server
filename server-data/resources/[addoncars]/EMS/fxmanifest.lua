3ef12a46349d306f93518f0b88181faa48112d64
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 03:43:19 +0700

    no message

---
+++ server-data/resources/[addoncars]/EMS/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
