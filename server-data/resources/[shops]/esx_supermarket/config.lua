b522fcf27ddb73fd3755fbe7ad8b808b583d6602
whysaputro <hendrawahyu26@gmail.com>
2020-06-26 12:38:34 +0700

    no message

---
+++ server-data/resources/[shops]/esx_supermarket/config.lua
@@ -0,0 +1,16 @@
+Config              = {}
+Config.DrawDistance = 100
+Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
+Config.Color        = {r = 0, g = 128, b = 255}
+Config.Type         = 1
+Config.Locale       = 'en'
+
+Config.Zones = {
+
+	Distributor = {
+		Items = {},
+		Pos = {
+			{x = 373.875,   y = 325.896,  z = 102.566}
+		}
+	}
+}
