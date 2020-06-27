6a680081886a79727177ee3a21592606dd342c08
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 15:19:30 +0700

    no message

--- server-data/resources/[custom scripts]/pillbox_beds/config.lua
+++ server-data/resources/[custom scripts]/pillbox_beds/config.lua
@@ -12,7 +12,7 @@ Config.BedList = {
 	{
 		text = '[E] Berbaring di kasur',
 		heading = -20.0,
-		objCoords  = {x = 321.58868, y = -567.9, z = 42.80},--pillbox ruang operasi
+		objCoords  = {x = 321.28868, y = -567.9, z = 42.80},--pillbox ruang operasi
 	},
 
 	{
