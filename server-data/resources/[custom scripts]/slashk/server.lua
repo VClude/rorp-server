03fa7552db22dfdc04ea9366e7be8de1a5e05a9a
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 13:51:28 +0700

    no message

--- server-data/resources/[custom scripts]/slashk/server.lua
+++ server-data/resources/[custom scripts]/slashk/server.lua
@@ -14,6 +14,6 @@
 --    You should have received a copy of the GNU General Public License
 --    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-TriggerEvent('es:addCommand', 'k', function(source, args, user)
+RegisterCommand('k', function(source, args, user)
 	TriggerClientEvent('KneelHU', source, {})
 end, {help = "Get down on your knees and put your hands on your head"})
