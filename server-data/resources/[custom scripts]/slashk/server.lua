a39107dd80ebfab5ab6c41041d1fc2e0efac7237
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 13:53:25 +0700

    no message

--- server-data/resources/[custom scripts]/slashk/server.lua
+++ server-data/resources/[custom scripts]/slashk/server.lua
@@ -16,4 +16,5 @@
 
 RegisterCommand('k', function(source, args, user)
 	TriggerClientEvent('KneelHU', source, {})
-end, {help = "Get down on your knees and put your hands on your head"})
+end, false)
+
