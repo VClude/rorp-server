e764bd663d71a24b39d940e4ba1c58abf67a06f7
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 15:36:27 +0700

    no message

--- server-data/resources/[custom scripts]/pillbox_beds/config.lua
+++ server-data/resources/[custom scripts]/pillbox_beds/config.lua
@@ -4,51 +4,52 @@ Config.MaxDistance = 1.5
 Config.BedList = {
 
 	{
-		text = '[E] Tidur di kasur',
+		text = 'Tekan ~g~[E]~s~ Tidur di kasur',
 		heading = -20.0,
 		objCoords  = {x = 315.5845, y = -566.36, z = 43.28},---pillbox ruang operasi
 	},
 	  ----451.3828 -356.163 -186.79572
 	{
-		text = '[E] Tidur di kasur',
+		text = 'Tekan ~g~[E]~s~ Tidur di kasur',
 		heading = -20.0,
 		objCoords  = {x = 321.02868, y = -568.21, z = 42.80},--pillbox ruang operasi
 	},
 
 	{
-		text = '[E] Tidur di kasur',
+		text = 'Tekan ~g~[E]~s~ Tidur di kasur',
 		heading = -20.0,
 		objCoords  = {x = 336.845856, y = -575.23, z = 43.10},--pillbox ruang mri
 	},
 
 	{
-		text = '[E] Tidur di kasur',
+		text = 'Tekan ~g~[E]~s~ Tidur di kasur',
 		heading = -20.0,
-		objCoords  = {x = 313.79, y = -578.95, z = 43.022},---pillbox ruang intensif
+		objCoords  = {x = 313.79, y = -578.95, z = 43.022},--pillbox ruang intensif
 	},
 
 	{
-		text = '~g~[E]~s~ Tidur di kasur',
+		text = 'Tekan ~g~[E]~s~ Tidur di kasur',
 		heading = -20.0,
-		objCoords  = {x = 309.34, y = -577.42, z = 43.02},---pillbox
+		objCoords  = {x = 309.34, y = -577.42, z = 43.02},--pillbox ruang intensif
 	},
 
 	{
-		text = '[E] Lie on the bed',
-		heading = 80.0,
-		objCoords  = {x = 334.09, y = -578.43, z = 43.01},--pillbox 
+		text = 'Tekan ~g~[E]~s~ Tidur di kasur',
+		heading = -20.0,
+		objCoords  = {x = 319.4, y = -581.02, z = 43.01},--pillbox ruang intensif
 	},
 
 	{
-		text = '[E] Lie on the bed',
+		text = 'Tekan ~g~[E]~s~ Tidur di kasur',
 		heading = -20.0,
-		objCoords  = {x = 323.64, y = -575.16, z = 43.02}, --- pillbox
+		objCoords  = {x = 324.17, y = -582.89, z = 43.01},--pillbox ruang intensif
 	},
 
+
 	{
-		text = '[E] Lie on the bed',
+		text = 'Tekan ~g~[E]~s~ Tidur di kasur',
 		heading = -20.0,
-		objCoords  = {x = 326.97, y = -576.229, z = 43.02}, ---pillbox
+		objCoords  = {x = 322.78, y = -587.09, z = 43.01},--pillbox ruang intensif
 	},
 
 	{
