a3a1466c72d684d32057c1034d48c52014ad19ec
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 15:01:30 +0700

    no message

--- server-data/resources/[custom scripts]/pillbox_beds/config.lua
+++ server-data/resources/[custom scripts]/pillbox_beds/config.lua
@@ -6,7 +6,7 @@ Config.BedList = {
 	{
 		text = '[E] Lie on the bed',
 		heading = -20.0,
-		objCoords  = {x = 356.73, y = -585.71, z = 43.11},---pillbox 
+		objCoords  = {x = 316.2, y = -566.5, z = 43.28},---pillbox 
 	},
 	  ----451.3828 -356.163 -186.79572
 	{
