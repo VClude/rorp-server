ba47c40cb3dbb16f7a1212f32720b63b2c3ba7d7
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 23:23:30 +0700

    no message

--- server-data/resources/[custom scripts]/nohelmet/nohelmet.lua
+++ server-data/resources/[custom scripts]/nohelmet/nohelmet.lua
@@ -1,12 +1,11 @@
 Citizen.CreateThread( function()
-	while true do
-	  Citizen.Wait(100)		
-	  local playerPed = GetPlayerPed(-1)
-	  local playerVeh = GetVehiclePedIsUsing(playerPed)
-	
-		if playerVeh ~= 0 then 
-			RemovePedHelmet(playerPed,true) 
-			SetPedConfigFlag(GetPlayerPed(-1), 35, false)
-		end
-	end	
+	if IsPedInAnyVehicle(PlayerPedId(), false) then
+		print('ada di dalam kendaraan')
+	end
+	-- local playerPed = GetPlayerPed(-1)
+	-- local playerVeh = GetVehiclePedIsUsing(playerPed)
+	-- 	if playerVeh ~= 0 then 
+	-- 		RemovePedHelmet(playerPed,true) 
+
+	-- 	end
 end)
