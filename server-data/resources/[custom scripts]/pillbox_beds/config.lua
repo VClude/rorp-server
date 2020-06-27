6e9dce4ce4b10fbeafae0f5b0882d0778c1fcb5f
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 15:32:42 +0700

    no message

--- server-data/resources/[custom scripts]/pillbox_beds/config.lua
+++ server-data/resources/[custom scripts]/pillbox_beds/config.lua
@@ -4,33 +4,33 @@ Config.MaxDistance = 1.5
 Config.BedList = {
 
 	{
-		text = '[E] Berbaring di kasur',
+		text = '[E] Tidur di kasur',
 		heading = -20.0,
 		objCoords  = {x = 315.5845, y = -566.36, z = 43.28},---pillbox ruang operasi
 	},
 	  ----451.3828 -356.163 -186.79572
 	{
-		text = '[E] Berbaring di kasur',
+		text = '[E] Tidur di kasur',
 		heading = -20.0,
 		objCoords  = {x = 321.02868, y = -568.21, z = 42.80},--pillbox ruang operasi
 	},
 
 	{
-		text = '[E] Berbaring di kasur',
+		text = '[E] Tidur di kasur',
 		heading = -20.0,
 		objCoords  = {x = 336.845856, y = -575.23, z = 43.10},--pillbox ruang mri
 	},
 
 	{
-		text = '[E] Berbaring di kasur',
+		text = '[E] Tidur di kasur',
 		heading = -20.0,
 		objCoords  = {x = 313.79, y = -578.95, z = 43.022},---pillbox ruang intensif
 	},
 
 	{
-		text = '[E] Lie on the bed',
-		heading = 80.0,
-		objCoords  = {x = 344.80, y = -581.12, z = 43.02},---pillbox
+		text = '~g~[E]~s~ Tidur di kasur',
+		heading = -20.0,
+		objCoords  = {x = 309.34, y = -577.42, z = 43.02},---pillbox
 	},
 
 	{
