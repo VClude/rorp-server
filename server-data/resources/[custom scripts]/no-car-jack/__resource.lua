b679e0589e9e38a5478a6265e7a376e6f1d782e8
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 14:48:37 +0700

    no message

--- server-data/resources/[custom scripts]/no-car-jack/__resource.lua
+++ server-data/resources/[custom scripts]/no-car-jack/__resource.lua
@@ -1,2 +1,15 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-client_scripts "NoCarJack.net.dll"
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX NoCarJack - by 0xNOP @ FiveM'
+
+version '1.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server/nocarjack_sv.lua'
+}
+
+client_scripts {
+	'client/nocarjack_cl.lua',
+	'cfg/nocarjack.lua'
+}
