9ad355c494d79efe100a2efa9a233b6907b24da7
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 20:32:22 +0700

    no message

--- server-data/resources/[custom scripts]/nohelmet/nohelmet.lua
+++ server-data/resources/[custom scripts]/nohelmet/nohelmet.lua
@@ -1,12 +1,13 @@
 Citizen.CreateThread( function()
-SetPedHelmet(GetPlayerPed(-1), false)
-
-	while true do
-		Citizen.Wait(900000)		
-		local playerPed = GetPlayerPed(-1)
-		local playerVeh = GetVehiclePedIsUsing(playerPed)
-
-		if playerVeh ~= 30000 then RemovePedHelmet(playerPed,true) end
-	end
+	SetPedHelmet(GetPlayerPed(-1), false)
 	
-end)
+		while true do
+			Citizen.Wait(900000)		
+			local playerPed = GetPlayerPed(-1)
+			local playerVeh = GetVehiclePedIsUsing(playerPed)
+	
+			if gPlayerVeh ~= 30000 then RemovePedHelmet(playerPed,true) end
+		end
+		
+	end)
+	
