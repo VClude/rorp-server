77645545b567f9ff91f257e7342839d82990354d
whysaputro <hendrawahyu26@gmail.com>
2020-06-27 20:40:17 +0700

    no message

--- server-data/resources/[custom scripts]/nohelmet/nohelmet.lua
+++ server-data/resources/[custom scripts]/nohelmet/nohelmet.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread( function()
-	while true do
-	  Citizen.Wait(0)		--Edit this if the resource is using too much resources.   
-	  local playerVeh = GetVehiclePedIsUsing(player)
-	  if playerVeh ~= 0 then SetPedConfigFlag(player, 35, false) end
-	end	
-  end)
+  while true do
+    Citizen.Wait(0)		--Edit this if the resource is using too much resources.   
+    local playerVeh = GetVehiclePedIsUsing(player)
+    if PlayerVeh ~= 0 then SetPedConfigFlag(player, 35, false) end
+  end	
+end)
