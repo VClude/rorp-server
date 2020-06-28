cfcf2ee263bd2a377b49e17e2599568685f53425
whysaputro <hendrawahyu26@gmail.com>
2020-06-28 19:33:01 +0700

    no message

--- server-data/resources/[jobs]/rorp_petani_k/client/main.lua
+++ server-data/resources/[jobs]/rorp_petani_k/client/main.lua
@@ -276,7 +276,7 @@ function ValidateCrops(plantCoord)
 end
 
 function GetZ(x, y)
-	local groundCheckHeights = { 156.0, 157.0, 158.0, 159.0, 160.0, 161.0, 162.0, 163.0, 164.0, 165.0, 166.0, 167.0, 168.0, 169.0, 170.0, 171.0 }
+	local groundCheckHeights = { 156.0, 157.0, 158.0, 159.0, 160.0, 161.0, 162.0, 163.0, 164.0, 165.0, 166.0, 167.0, 168.0, 169.0, 170.0, 171.0, 172.0, 173.0, 174.0 }
 
 	for i, height in ipairs(groundCheckHeights) do
 		local foundGround, z = GetGroundZFor_3dCoord(x, y, height)
