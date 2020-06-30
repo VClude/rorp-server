678224fa6e932d05c7dd6f27c82d758dfeaaccb0
Debian <debian@packer-output-6c4d6372-f112-4ad7-8cc7-7e3a22339bff>
2020-06-30 20:32:41 +0000

    mobil WIBU & tambah resource redzone

---
+++ server-data/resources/[addoncars]/amgt3/__resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version 'ee782as-63ca-442c-a67b-a6s7d8g12'
+
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
---
+++ server-data/resources/[custom scripts]/redzone/client.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+local blips = AddBlipForRadius(552.03, 5430.21, 670.21 , 3700.0) 
+
+SetBlipHighDetail(blips, true)
+SetBlipColour(blips, 1)
+SetBlipAlpha (blips, 128)
+end)
