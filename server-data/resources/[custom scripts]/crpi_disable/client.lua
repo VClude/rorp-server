289abbb049e3d3c23668763bc2b83b3e1628e4f4
whysaputro <hendrawahyu26@gmail.com>
2020-06-30 10:17:28 +0700

    no message

--- server-data/resources/[custom scripts]/crpi_disable/client.lua
+++ server-data/resources/[custom scripts]/crpi_disable/client.lua
@@ -6,8 +6,8 @@ Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
-	end)
-end
+	end
+end)
 
 
 local gates = {
