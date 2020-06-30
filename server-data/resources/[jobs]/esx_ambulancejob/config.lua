da1e13d25dcc2351ae6fe16edb61dde5745ec842
whysaputro <hendrawahyu26@gmail.com>
2020-06-30 08:14:02 +0700

    no message

--- server-data/resources/[jobs]/esx_ambulancejob/config.lua
+++ server-data/resources/[jobs]/esx_ambulancejob/config.lua
@@ -75,12 +75,14 @@ Config.Hospitals = {
 				From = vector3(329.77, -601.06, 42.28),
 				To = {coords = vector3(343.54, -585.68, 73.16), heading = 251.4},
 				Marker = {type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
+				Prompt = _U('fast_travel')
 			},
 
 			{
 				From = vector3(338.49, -583.85, 73.16),
 				To = {coords = vector3(332.07, -595.59, 43.28), heading = 64.46},
 				Marker = {type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
+				Prompt = _U('fast_travel')
 			}
 		}
 
