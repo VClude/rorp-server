7f65657bdc79fd7fa53b3d2dd09a4083d4ea47ad
whysaputro <hendrawahyu26@gmail.com>
2020-07-02 19:01:51 +0700

    no message

--- server-data/resources/[jobs]/rorp_miner/client/main.lua
+++ server-data/resources/[jobs]/rorp_miner/client/main.lua
@@ -58,7 +58,7 @@ end)
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
-		local zones, currentZone, currentZoneIndex, isInMarker = {}
+		local zones, currentZone, isInMarker = {}
 		local letSleep, playerPed = true, PlayerPedId()
         local playerCoords = GetEntityCoords(playerPed)
         
@@ -77,16 +77,17 @@ Citizen.CreateThread(function()
 				end
 
 				if distance < v.Size.x then
-					letSleep, isInMarker, currentZone, currentZoneIndex = false, true, v, k
+					letSleep, isInMarker, currentZone = false, true, v
 					break
 				end
 			end
 
-			-- if IsControlJustReleased(0, 38) and not menuIsShowed and isInMarker then
-			-- 	if onDuty or currentZone.Type == 'cloakroom' or PlayerData.job.name == 'reporter' then
-			-- 		TriggerEvent('esx_jobs:action', PlayerData.job.name, currentZone, currentZoneIndex)
-			-- 	end
-			-- end
+			if IsControlJustReleased(0, 38) and not menuIsShowed and isInMarker then
+                if onDuty or currentZone.Type == 'cloakroom' then
+                    print(currentZone.Type)
+					-- TriggerEvent('esx_jobs:action', PlayerData.job.name, currentZone, currentZoneIndex)
+				end
+			end
 
 			-- -- hide or show top left zone hints
 			-- if isInMarker and not menuIsShowed then
