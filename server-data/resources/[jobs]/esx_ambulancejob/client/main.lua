e8e9d95229136ff619399247e32ec0a85389f9f9
whysaputro <hendrawahyu26@gmail.com>
2020-07-05 00:57:54 +0700

    no message

--- server-data/resources/[jobs]/esx_ambulancejob/client/main.lua
+++ server-data/resources/[jobs]/esx_ambulancejob/client/main.lua
@@ -54,16 +54,6 @@ AddEventHandler('esx:setJob', function(job)
 	end
 end)
 
-function killPlayer()
-	SetEntityHealth(GetPlayerPed(-1),0)
-	playerDied()
-end
-
-function playerDied()
-	TriggerServerEvent('esx_ambulancejob:setDeathStatus', true)
-	isDead = true
-end
-
 AddEventHandler('esx:onPlayerSpawn', function()
 	isDead = false
 
@@ -72,14 +62,14 @@ AddEventHandler('esx:onPlayerSpawn', function()
 
 		if Config.AntiCombatLog then
 			while not PlayerLoaded do
-				Citizen.Wait(1000)
+				Citizen.Wait(3000)
 			end
 
 			ESX.TriggerServerCallback('esx_ambulancejob:getDeathStatus', function(shouldDie)
 				if shouldDie then
 					exports['mythic_notify']:DoCustomHudText('inform', _U('combatlog_message'), 5000)
-					-- RemoveItemsAfterRPDeath()
-					killPlayer()
+					SetEntityHealth(GetPlayerPed(-1),0)
+					-- RemoveItemsAfterRPDeath()					
 				end
 			end)
 		end
