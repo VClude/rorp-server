ee0f2efd704fc0a77f069421f3872b691cb7aefe
whysaputro <hendrawahyu26@gmail.com>
2020-07-05 19:47:51 +0700

    no message

--- server-data/resources/[operations]/esx_realweapons/config.lua
+++ server-data/resources/[operations]/esx_realweapons/config.lua
@@ -7,7 +7,7 @@ Config.RealWeapons = {
 	{name = 'WEAPON_KNIFE', 			bone = 24818, x = 65536.0, y = 65536.0, z = 65536.0, xRot = 0.0, yRot = 0.0, zRot = 0.0, category = 'melee', 		model = 'prop_w_me_knife_01'},
 	{name = 'WEAPON_NIGHTSTICK', 		bone = 24818, x = 65536.0, y = 65536.0, z = 65536.0, xRot = 0.0, yRot = 0.0, zRot = 0.0, category = 'melee', 		model = 'w_me_nightstick'},
 	{name = 'WEAPON_HAMMER', 			bone = 24818, x = 65536.0, y = 65536.0, z = 65536.0, xRot = 0.0, yRot = 0.0, zRot = 0.0, category = 'melee', 		model = 'prop_tool_hammer'},
-	{name = 'WEAPON_BAT', 				bone = 58271, x = -0.01, y = 0.1,  z = -0.07, xRot = -110.0,  yRot = 0.10, zRot = 0.0, category = 'melee', 		model = 'w_me_bat'},
+	{name = 'WEAPON_BAT', 				bone = 58271, x = -0.01, y = 0.1,  z = -0.07, xRot = -55.0,  yRot = 0.80, zRot = 0.0, category = 'melee', 		model = 'w_me_bat'},
 	{name = 'WEAPON_GOLFCLUB', 			bone = 24818, x = 65536.0, y = 65536.0, z = 65536.0, xRot = 0.0, yRot = 0.0, zRot = 0.0, category = 'melee', 		model = 'w_me_gclub'},
 	{name = 'WEAPON_CROWBAR', 			bone = 24818, x = 65536.0, y = 65536.0, z = 65536.0, xRot = 0.0, yRot = 0.0, zRot = 0.0, category = 'melee', 		model = 'w_me_crowbar'},
 	{name = 'WEAPON_BOTTLE', 			bone = 24818, x = 65536.0, y = 65536.0, z = 65536.0, xRot = 0.0, yRot = 0.0, zRot = 0.0, category = 'melee', 		model = 'prop_w_me_bottle'},
