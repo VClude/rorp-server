6570b3f5975ad84bb31e5aaacd1fecbdf9974ed5
whysaputro <hendrawahyu26@gmail.com>
2020-07-07 19:44:05 +0700

    no message

--- server-data/resources/[custom scripts]/ELS-FiveM/shared/shared.lua
+++
@@ -1,9 +0,0 @@
-function debugPrint(msg, force, inLoop)
-    local prefix = IsDuplicityVersion() and '(server)' or '(client)'
-    if printDebugInformation or force then
-        print(prefix .. ' ELS-FiveM: ' .. msg)
-        if inLoop then
-            Citizen.Wait(500)
-        end
-    end
-end
