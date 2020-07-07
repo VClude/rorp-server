6724334a164c78bfddd93740cd0890683025a87a
whysaputro <hendrawahyu26@gmail.com>
2020-07-08 04:41:13 +0700

    no message

--- server-data/resources/[custom scripts]/solokick/client.lua
+++ server-data/resources/[custom scripts]/solokick/client.lua
@@ -1,16 +1,16 @@
-local wait = 2*60*1000
-Citizen.CreateThread(function()
-	Citizen.Wait(wait) -- Delay first spawn.
-	while true do
-		TriggerServerEvent('sendSession:PlayerNumber', #GetActivePlayers())
-		Citizen.Wait(wait)
-	end
-end)
+-- local wait = 2*60*1000
+-- Citizen.CreateThread(function()
+-- 	Citizen.Wait(wait) -- Delay first spawn.
+-- 	while true do
+-- 		TriggerServerEvent('sendSession:PlayerNumber', #GetActivePlayers())
+-- 		Citizen.Wait(wait)
+-- 	end
+-- end)
 
-AddEventHandler("playerConnecting",function(name,setMessage, deferrals)
-	deferrals.defer()
+-- AddEventHandler("playerConnecting",function(name,setMessage, deferrals)
+-- 	deferrals.defer()
 
-	DropPlayer( source,"No steam login." )
-	Citizen.Wait(1000)
-	deferrals.done("No steam login" )
-end)
+-- 	DropPlayer( source,"No steam login." )
+-- 	Citizen.Wait(1000)
+-- 	deferrals.done("No steam login" )
+-- end)
--- server-data/resources/[custom scripts]/solokick/server.lua
+++ server-data/resources/[custom scripts]/solokick/server.lua
@@ -1,29 +1,59 @@
-RegisterServerEvent('sendSession:PlayerNumber')
-AddEventHandler('sendSession:PlayerNumber', function(clientPlayerNumber)
-	if source ~= nil then
-		local serverPlayerNumber = #GetPlayers()
-		if serverPlayerNumber-clientPlayerNumber > 5 then 
-			DropPlayer(source, '[Kick] Solo Session') -- Kick player
-			print("^1[SOLO-KICK] Server-"..serverPlayerNumber.." / Client-"..clientPlayerNumber.."^0") -- Debug
-		end
-	end
-end)
+-- RegisterServerEvent('sendSession:PlayerNumber')
+-- AddEventHandler('sendSession:PlayerNumber', function(clientPlayerNumber)
+-- 	if source ~= nil then
+-- 		local serverPlayerNumber = #GetPlayers()
+-- 		if serverPlayerNumber-clientPlayerNumber > 5 then 
+-- 			DropPlayer(source, '[Kick] Solo Session') -- Kick player
+-- 			print("^1[SOLO-KICK] Server-"..serverPlayerNumber.." / Client-"..clientPlayerNumber.."^0") -- Debug
+-- 		end
+-- 	end
+-- end)
 
--- Check for update
-local CurrentVersion = [[5.0
-]]
-PerformHttpRequest('https://raw.githubusercontent.com/chaixshot/fivem/master/solokick/version', function(Error, NewestVersion, Header)
-	if CurrentVersion ~= NewestVersion then
-		print('\n')
-		print('##')
-		print('## Solo Kick')
-		print('##')
-		print('## Current Version: ' .. CurrentVersion)
-		print('## Newest Version: ' .. NewestVersion)
-		print('##')
-		print('## Download')
-		print('## https://github.com/chaixshot/fivem/tree/master/solokick')
-		print('##')
-		print('\n')
-	end
-end)
+-- -- Check for update
+-- local CurrentVersion = [[5.0
+-- ]]
+-- PerformHttpRequest('https://raw.githubusercontent.com/chaixshot/fivem/master/solokick/version', function(Error, NewestVersion, Header)
+-- 	if CurrentVersion ~= NewestVersion then
+-- 		print('\n')
+-- 		print('##')
+-- 		print('## Solo Kick')
+-- 		print('##')
+-- 		print('## Current Version: ' .. CurrentVersion)
+-- 		print('## Newest Version: ' .. NewestVersion)
+-- 		print('##')
+-- 		print('## Download')
+-- 		print('## https://github.com/chaixshot/fivem/tree/master/solokick')
+-- 		print('##')
+-- 		print('\n')
+-- 	end
+-- end)
+
+local function OnPlayerConnecting(name, setKickReason, deferrals)
+    local player = source
+    local steamIdentifier
+    local identifiers = GetPlayerIdentifiers(player)
+    deferrals.defer()
+
+    -- mandatory wait!
+    Wait(0)
+
+    deferrals.update(string.format("Hello %s. Your Steam ID is being checked.", name))
+
+    for _, v in pairs(identifiers) do
+        if string.find(v, "steam") then
+            steamIdentifier = v
+            break
+        end
+    end
+
+    -- mandatory wait!
+    Wait(0)
+
+    if not steamIdentifier then
+        deferrals.done("You are not connected to Steam.")
+    else
+        deferrals.done()
+    end
+end
+
+AddEventHandler("playerConnecting", OnPlayerConnecting)
